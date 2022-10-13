c29ef77829594d2aee34dd89144db0112a86db8e
ang95 <geman.apaza.n@gmail.com>
2022-10-13 03:19:58 -0500

    create uuID function

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -5,3 +5,10 @@ export const CURRENT_DATE = currentDate.getDate() + "/"
   + currentDate.getHours() + ":"
   + currentDate.getMinutes() + ":"
   + currentDate.getSeconds();
+
+export const UuID = () => {
+  const n = Date.now();
+  const r = Math.random();
+  const s = String(n) + String(~~(r*9e4)+1e4);
+  return `${s.slice(0,8)}-${s.slice(8,12)}-4${s.slice(12,15)}-${[8,9,'a','b'][~~(r*3)]}${s.slice(15,18)}-${s.slice(s.length-12)}`;
+};
